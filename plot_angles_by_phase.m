c1487431dea85ea3bc431297b0cd3a2b3a6fac3e
Gilles <gillesdelbecq@hotmail.fr>
2023-09-22 19:32:47 +0200

    Update plot_angles_by_phase.m

--- plot_angles_by_phase.m
+++ plot_angles_by_phase.m
@@ -1,4 +1,4 @@
-function plot_all_angles_by_phase(angles, phase)
+function plot_angles_by_phase(angles, phase)
     % Assumptions: 
     % angles is a vector of length N (here N = 15978)
     % phase is a vector of length N with values between -pi and pi
@@ -20,18 +20,11 @@ function plot_all_angles_by_phase(angles, phase)
         mean_angles(i) = mean(angles_by_phase{i});
     end
     
-    % Plot all angles for each phase with transparency
+    % Plot the mean angles against the normalized phase
     normalized_phase = linspace(0, 100, num_bins);  % Normalized phase in percentage
-    hold on;
-    for i = 1:num_bins
-        plot(normalized_phase(i)*ones(size(angles_by_phase{i})), angles_by_phase{i}, '.r', 'MarkerSize', 8, 'Color', [1 0 0 0.1]);  % 0.1 is the alpha (transparency)
-    end
-    
-    % Plot the mean angles on top
-    plot(normalized_phase, mean_angles, 'o-', 'Color', [1 0 0]);
+    plot(normalized_phase, mean_angles);
     xlabel('Normalized Phase (%)');
-    ylabel('Angle');
-    title('All Angles and Mean Angle by Phase');
+    ylabel('Mean Angle');
+    title('Mean Angle by Phase');
     grid on;
-    hold off;
 end
