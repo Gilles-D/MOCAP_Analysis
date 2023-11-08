d11ca3285031873043b41023ac24d5acec03aaba
Gilles <gillesdelbecq@hotmail.fr>
2023-11-08 18:05:41 +0100

    Update detect_optotagged_units.m

--- detect_optotagged_units.m
+++ detect_optotagged_units.m
@@ -1,10 +1,24 @@
-function optotagged_units = detect_optotagged_units(filename,optotagged_threshold)
+% function optotagged_units = detect_optotagged_units(filename,optotagged_threshold)
+%     % Lisez le fichier Excel
+%     data = readtable(filename);
+% 
+%     % Vérifiez les unités avec un reliability_scores >= optotagged_threshold
+%     selected_rows = data.reliability_scores >= optotagged_threshold;
+% 
+%     % Récupérez les numéros des unités optotaggées
+%     optotagged_units = data.units(selected_rows);
+% end
+
+function optotagged_indices = detect_optotagged_units(filename, zscore_threshold, success_rate_threshold)
     % Lisez le fichier Excel
     data = readtable(filename);
 
-    % Vérifiez les unités avec un reliability_scores >= optotagged_threshold
-    selected_rows = data.reliability_scores >= optotagged_threshold;
+    % Vérifiez les unités avec un Zscore >= zscore_threshold et un pourcentage de succès >= success_rate_threshold
+    selected_rows = (data.('Z_score') >= zscore_threshold) & (data.('x_Success') >= success_rate_threshold);
 
-    % Récupérez les numéros des unités optotaggées
-    optotagged_units = data.units(selected_rows);
+
+    % Récupérez les unités qui répondent aux critères
+%     optotagged_units = data.units(selected_rows);
+    optotagged_indices = find(selected_rows);
 end
+
